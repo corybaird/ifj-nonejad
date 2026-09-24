d6235c5a84361805af7f8365a3d45eba812b6f5f
corybaird <31351628+corybaird@users.noreply.github.com>
2026-09-24 13:00:53 +0900

    FIX dummy marker filtering in figure4 and figure5 scripts

--- replication/matlab/replication_of_submission/figure4.m
+++ replication/matlab/replication_of_submission/figure4.m
@@ -1,37 +1,47 @@
 
 clear all
 clc
+close all
 
-addpath('functions')
-addpath('data')
+if exist('functions', 'dir'), addpath('functions'); end
+if exist('data', 'dir'), addpath('data'); end
+if exist('results', 'dir'), addpath('results'); end
+if exist('../functions', 'dir'), addpath('../functions'); end
+if exist('../data', 'dir'), addpath('../data'); end
+if exist('../results', 'dir'), addpath('../results'); end
 
 mc=xlsread('results.xls','figure4');
+mc=mc(1:18,:);
 
 subplot(2,2,1)
-plot([1,1],get(gca,'ylim'))
+scatter(mc(:,1),mc(:,2),50,'b','filled')
 hold on
-plot(xlim,[0.1,0.1])
-hold on 
-scatter(mc(:,1),mc(:,2))   
+plot([1,1],get(gca,'ylim'),'k:')
+plot(xlim,[0.1,0.1],'k--','LineWidth',2)
+xlabel('TU'); ylabel('CW p-value');
+title('(a): Expansion periods in the United States as defined by NBER.');
 
 subplot(2,2,2)
-plot([1,1],get(gca,'ylim'))
+scatter(mc(:,1),mc(:,3),50,'b','filled')
 hold on
-plot(xlim,[0.1,0.1])
-hold on 
-scatter(mc(:,1),mc(:,3))
+plot([1,1],get(gca,'ylim'),'k:')
+plot(xlim,[0.1,0.1],'k--','LineWidth',2)
+xlabel('TU'); ylabel('DM p-value');
+title('(b): Expansion periods in the United States as defined by NBER.');
 
 subplot(2,2,3)
-plot([1,1],get(gca,'ylim'))
+scatter(mc(:,4),mc(:,5),50,'b','filled')
 hold on
-plot(xlim,[0.1,0.1])
-hold on 
-scatter(mc(:,4),mc(:,5))
+plot([1,1],get(gca,'ylim'),'k:')
+plot(xlim,[0.1,0.1],'k--','LineWidth',2)
+xlabel('TU'); ylabel('CW p-value');
+title('(c): Recession periods in the United States as defined by NBER.');
 
 subplot(2,2,4)
-plot([1,1],get(gca,'ylim'))
+scatter(mc(:,4),mc(:,6),50,'b','filled')
 hold on
-plot(xlim,[0.1,0.1])
-hold on 
-scatter(mc(:,4),mc(:,6))
+plot([1,1],get(gca,'ylim'),'k:')
+plot(xlim,[0.2,0.2],'k--','LineWidth',2)
+xlabel('TU'); ylabel('DM p-value');
+title('(d): Recession periods in the United States as defined by NBER.');
  
