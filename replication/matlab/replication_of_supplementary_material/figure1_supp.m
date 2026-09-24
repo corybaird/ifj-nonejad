f175da6f1305de31f0e449746b50c1187ae30f1d
corybaird <31351628+corybaird@users.noreply.github.com>
2026-09-24 13:01:01 +0900

    UPDATE supplementary figures 1 through 3 scripts

--- replication/matlab/replication_of_supplementary_material/figure1_supp.m
+++ replication/matlab/replication_of_supplementary_material/figure1_supp.m
@@ -1,14 +1,17 @@
-
 clear all
+close all
 clc
 
-addpath('functions')
-addpath('data')
-
-% mtstream=RandStream('mt19937ar');
-% RandStream.setDefaultStream(mtstream);
+if exist('functions', 'dir'), addpath('functions'); end
+if exist('data', 'dir'), addpath('data'); end
+if exist('results', 'dir'), addpath('results'); end
+if exist('../functions', 'dir'), addpath('../functions'); end
+if exist('../data', 'dir'), addpath('../data'); end
+if exist('../results', 'dir'), addpath('../results'); end
 
 mc=xlsread('results.xls','figure1_supp');
+
+fig = figure('Visible', 'off', 'Position', [100 100 900 700]);
 
 vtid=(1:420)';
 xlimits=[1,420];
@@ -18,19 +21,26 @@ ma=[vtid,vb];
 vstart=find(ma(:,end)==1);
 vend=(find(ma(:,end)==-1)-1)+2;
 Recessions=[vtid(vstart,1),vtid(vend,1)];
+
 subplot(3,2,1)
 plot(mc(:,1))
 axis tight;
 recession_bars;
-plot(vtid,mc(:,1))
+plot(vtid,mc(:,1), 'b', 'LineWidth', 1.2)
 axis tight; 
+xlim([1, 420]); ylim([0, 0.4]);
+xlabel('Time'); ylabel('Value');
+title('(a): Results using the estimated correlation coefficient matrix.');
+
 subplot(3,2,2)
-plot(mc(:,3:6))
-legend('TN','FP','FN','TP')
+plot(mc(:,3:6), 'LineWidth', 1.0)
+legend('TN','FP','FN','TP', 'Location', 'best')
 hold on
-plot(xlim,[0.1,0.1])
-hold on
-plot(xlim,[0.8,0.8])
+plot(xlim,[0.1,0.1], 'k:')
+plot(xlim,[0.8,0.8], 'k:')
+xlim([0, 420]); ylim([0, 1.0]);
+xlabel('Time'); ylabel('Rate');
+title('(b): Results using the estimated correlation coefficient matrix.');
 
 vtid=(1:420)';
 xlimits=[1,420];
@@ -40,18 +50,26 @@ ma=[vtid,vb];
 vstart=find(ma(:,end)==1);
 vend=(find(ma(:,end)==-1)-1)+2;
 Recessions=[vtid(vstart,1),vtid(vend,1)];
+
 subplot(3,2,3)
 plot(mc(:,8))
 axis tight;
 recession_bars;
-plot(vtid,mc(:,8))
+plot(vtid,mc(:,8), 'b', 'LineWidth', 1.2)
 axis tight; 
+xlim([1, 420]); ylim([0, 0.5]);
+xlabel('Time'); ylabel('Value');
+title('(c): High-correlation scenario.');
+
 subplot(3,2,4)
-plot(mc(:,10:13))
+plot(mc(:,10:13), 'LineWidth', 1.0)
+legend('TN','FP','FN','TP', 'Location', 'best')
 hold on
-plot(xlim,[0.1,0.1])
-hold on
-plot(xlim,[0.8,0.8])
+plot(xlim,[0.1,0.1], 'k:')
+plot(xlim,[0.8,0.8], 'k:')
+xlim([0, 420]); ylim([0, 1.0]);
+xlabel('Time'); ylabel('Rate');
+title('(d): High-correlation scenario.');
 
 vtid=(1:420)';
 xlimits=[1,420];
@@ -61,17 +79,33 @@ ma=[vtid,vb];
 vstart=find(ma(:,end)==1);
 vend=(find(ma(:,end)==-1)-1)+2;
 Recessions=[vtid(vstart,1),vtid(vend,1)];
+
 subplot(3,2,5)
 plot(mc(:,15))
 axis tight;
 recession_bars;
-plot(vtid,mc(:,15))
+plot(vtid,mc(:,15), 'b', 'LineWidth', 1.2)
 axis tight; 
-subplot(3,2,6)
-plot(mc(:,17:20))
-hold on
-plot(xlim,[0.1,0.1])
-hold on
-plot(xlim,[0.8,0.8])
+xlim([1, 420]); ylim([0, 0.3]);
+xlabel('Time'); ylabel('Value');
+title('(e): Low-correlation scenario.');
 
- 
+subplot(3,2,6)
+plot(mc(:,17:20), 'LineWidth', 1.0)
+legend('TN','FP','FN','TP', 'Location', 'best')
+hold on
+plot(xlim,[0.1,0.1], 'k:')
+plot(xlim,[0.8,0.8], 'k:')
+xlim([0, 420]); ylim([0, 1.0]);
+xlabel('Time'); ylabel('Rate');
+title('(f): Low-correlation scenario.');
+
+if exist('../plots', 'dir')
+    out_dir = '../plots';
+else
+    out_dir = 'plots';
+end
+set(fig, 'PaperPositionMode', 'auto');
+saveas(fig, fullfile(out_dir, 'figure1_supp_reproduced.png'));
+saveas(fig, fullfile(out_dir, 'figure1_supp.fig'));
+close(fig);
