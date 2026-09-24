a327862c035e83df24b8fe1eb4b0bdf5115a6509
corybaird <31351628+corybaird@users.noreply.github.com>
2026-09-24 13:01:06 +0900

    UPDATE supplementary figures 4 through 6 scripts

--- replication/matlab/replication_of_supplementary_material/figure4_supp.m
+++ replication/matlab/replication_of_supplementary_material/figure4_supp.m
@@ -1,40 +1,56 @@
-
 clear all
+close all
 clc
 
-% mtstream=RandStream('mt19937ar');
-% RandStream.setDefaultStream(mtstream);
-
-addpath('functions')
-addpath('data')
+script_dir = fileparts(mfilename('fullpath'));
+if isempty(script_dir), script_dir = pwd; end
+addpath(fullfile(script_dir, '..', 'functions'));
+addpath(fullfile(script_dir, '..', 'data'));
+addpath(fullfile(script_dir, '..', 'results'));
 
 mc=xlsread('results.xls','figure4_supp');
+mc=mc(1:18, :);
+
+fig = figure('Visible', 'off', 'Position', [100 100 800 600]);
 
 subplot(2,2,1)
-plot([1,1],get(gca,'ylim'))
+scatter(mc(:,1),mc(:,2), 50, 'b', 'filled')   
 hold on
-plot(xlim,[0.1,0.1])
-hold on 
-scatter(mc(:,1),mc(:,2))   
+plot([1,1],[0, 1.0], 'k:')
+plot(xlim,[0.1,0.1], 'k--', 'LineWidth', 2)
+xlim([0.95, 1.02]); ylim([0, 1.0]);
+xlabel('TU'); ylabel('CW p-value');
+title('(a): 1990m1-2024m12 out-of-sample period.');
 
 subplot(2,2,2)
-plot([1,1],get(gca,'ylim'))
-hold on
-plot(xlim,[0.1,0.1])
+scatter(mc(:,1),mc(:,3), 50, 'b', 'filled')
 hold on 
-scatter(mc(:,1),mc(:,3))
+plot([1,1],[0, 1.0], 'k:')
+plot(xlim,[0.1,0.1], 'k--', 'LineWidth', 2)
+xlim([0.95, 1.02]); ylim([0, 1.0]);
+xlabel('TU'); ylabel('DM p-value');
+title('(b): 1990m1-2024m12 out-of-sample period.');
 
 subplot(2,2,3)
-plot([1,1],get(gca,'ylim'))
-hold on
-plot(xlim,[0.1,0.1])
+scatter(mc(:,4),mc(:,5), 50, 'b', 'filled')
 hold on 
-scatter(mc(:,4),mc(:,5))
+plot([1,1],[0, 1.0], 'k:')
+plot(xlim,[0.1,0.1], 'k--', 'LineWidth', 2)
+xlim([0.95, 1.02]); ylim([0, 1.0]);
+xlabel('TU'); ylabel('CW p-value');
+title('(c): 1990m1-2019m12 out-of-sample period.');
 
 subplot(2,2,4)
-plot([1,1],get(gca,'ylim'))
-hold on
-plot(xlim,[0.1,0.1])
+scatter(mc(:,4),mc(:,6), 50, 'b', 'filled')
 hold on 
-scatter(mc(:,4),mc(:,6))
- 
+plot([1,1],[0, 1.0], 'k:')
+plot(xlim,[0.1,0.1], 'k--', 'LineWidth', 2)
+xlim([0.95, 1.02]); ylim([0, 1.0]);
+xlabel('TU'); ylabel('DM p-value');
+title('(d): 1990m1-2019m12 out-of-sample period.');
+
+out_dir = fullfile(script_dir, '..', 'plots');
+set(fig, 'PaperPositionMode', 'auto');
+saveas(fig, fullfile(out_dir, 'figure4_supp_reproduced.png'));
+saveas(fig, fullfile(out_dir, 'figure4_supp.fig'));
+close(fig);
--- replication/matlab/replication_of_supplementary_material/figure6_supp.m
+++ replication/matlab/replication_of_supplementary_material/figure6_supp.m
@@ -1,40 +1,56 @@
-
 clear all
+close all
 clc
 
-% mtstream=RandStream('mt19937ar');
-% RandStream.setDefaultStream(mtstream);
-
-addpath('functions')
-addpath('data')
+script_dir = fileparts(mfilename('fullpath'));
+if isempty(script_dir), script_dir = pwd; end
+addpath(fullfile(script_dir, '..', 'functions'));
+addpath(fullfile(script_dir, '..', 'data'));
+addpath(fullfile(script_dir, '..', 'results'));
 
 mc=xlsread('results.xls','figure6_supp');
+mc=mc(1:18, :);
+
+fig = figure('Visible', 'off', 'Position', [100 100 800 600]);
 
 subplot(2,2,1)
-plot([1,1],get(gca,'ylim'))
+scatter(mc(:,1),mc(:,2), 50, 'b', 'filled')   
 hold on
-plot(xlim,[0.1,0.1])
-hold on 
-scatter(mc(:,1),mc(:,2))   
+plot([1,1],[0, 0.50], 'k:')
+plot(xlim,[0.1,0.1], 'k--', 'LineWidth', 2)
+xlim([0.95, 1.02]); ylim([0, 0.50]);
+xlabel('TU'); ylabel('CW p-value');
+title('(a): Hyperparameter values on the prior for \pi_{t+1} are \alpha=0.10 and \beta=0.10.');
 
 subplot(2,2,2)
-plot([1,1],get(gca,'ylim'))
-hold on
-plot(xlim,[0.1,0.1])
+scatter(mc(:,3),mc(:,4), 50, 'b', 'filled')
 hold on 
-scatter(mc(:,3),mc(:,4))
+plot([1,1],[0.04, 0.14], 'k:')
+plot(xlim,[0.1,0.1], 'k--', 'LineWidth', 2)
+xlim([0.95, 1.02]); ylim([0.04, 0.14]);
+xlabel('TU'); ylabel('CW p-value');
+title('(b): Hyperparameter values on the prior for \pi_{t+1} are \alpha=2 and \beta=2.');
 
 subplot(2,2,3)
-plot([1,1],get(gca,'ylim'))
-hold on
-plot(xlim,[0.1,0.1])
+scatter(mc(:,5),mc(:,6), 50, 'b', 'filled')
 hold on 
-scatter(mc(:,5),mc(:,6))
+plot([1,1],[0.04, 0.14], 'k:')
+plot(xlim,[0.1,0.1], 'k--', 'LineWidth', 2)
+xlim([0.95, 1.02]); ylim([0.04, 0.14]);
+xlabel('TU'); ylabel('CW p-value');
+title('(c): Hyperparameter values on the prior for \pi_{t+1} are \alpha=20 and \beta=1.');
 
 subplot(2,2,4)
-plot([1,1],get(gca,'ylim'))
-hold on
-plot(xlim,[0.1,0.1])
+scatter(mc(:,7),mc(:,8), 50, 'b', 'filled')
 hold on 
-scatter(mc(:,7),mc(:,8))
- 
+plot([1,1],[0.04, 0.14], 'k:')
+plot(xlim,[0.1,0.1], 'k--', 'LineWidth', 2)
+xlim([0.95, 1.02]); ylim([0.04, 0.14]);
+xlabel('TU'); ylabel('CW p-value');
+title('(d): Hyperparameter values on the prior for \pi_{t+1} are \alpha=20 and \beta=0.10.');
+
+out_dir = fullfile(script_dir, '..', 'plots');
+set(fig, 'PaperPositionMode', 'auto');
+saveas(fig, fullfile(out_dir, 'figure6_supp_reproduced.png'));
+saveas(fig, fullfile(out_dir, 'figure6_supp.fig'));
+close(fig);
