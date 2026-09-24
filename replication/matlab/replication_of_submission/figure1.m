cda54446c4342c58f04cd8bfe6ea09914c13bc1e
corybaird <31351628+corybaird@users.noreply.github.com>
2026-09-24 13:00:46 +0900

    FIX path resolution and export in figure1 scripts

--- replication/matlab/replication_of_submission/figure1.m
+++ replication/matlab/replication_of_submission/figure1.m
@@ -1,37 +1,49 @@
 
 clear all
-clc
+close all
 
-addpath('functions')
-addpath('data')
+if exist('functions', 'dir'), addpath('functions'); end
+if exist('data', 'dir'), addpath('data'); end
+if exist('results', 'dir'), addpath('results'); end
+if exist('../functions', 'dir'), addpath('../functions'); end
+if exist('../data', 'dir'), addpath('../data'); end
+if exist('../results', 'dir'), addpath('../results'); end
 
 mc=xlsread('results.xls','figure1');
 
 subplot(2,2,1)
-plot([1,1],get(gca,'ylim'))
+scatter(mc(:,1),mc(:,2),50,'b','filled')
 hold on
-plot(xlim,[0.1,0.1])
-hold on 
-scatter(mc(:,1),mc(:,2))   
+plot([1,1],[0, 1.0],'k:')
+plot(xlim,[0.1,0.1],'k--','LineWidth',2)
+xlim([0.945, 0.98]); ylim([0, 1.0]);
+xlabel('TU'); ylabel('CW p-value');
+title('(a): 1990m1-2024m12 out-of-sample period.');
 
 subplot(2,2,2)
-plot([1,1],get(gca,'ylim'))
+scatter(mc(:,1),mc(:,3),50,'b','filled')
 hold on
-plot(xlim,[0.1,0.1])
-hold on 
-scatter(mc(:,1),mc(:,3))
+plot([1,1],[0, 1.0],'k:')
+plot(xlim,[0.1,0.1],'k--','LineWidth',2)
+xlim([0.945, 0.98]); ylim([0, 1.0]);
+xlabel('TU'); ylabel('DM p-value');
+title('(b): 1990m1-2024m12 out-of-sample period.');
 
 subplot(2,2,3)
-plot([1,1],get(gca,'ylim'))
+scatter(mc(:,4),mc(:,5),50,'b','filled')
 hold on
-plot(xlim,[0.1,0.1])
-hold on 
-scatter(mc(:,4),mc(:,5))
+plot([1,1],[0, 1.0],'k:')
+plot(xlim,[0.1,0.1],'k--','LineWidth',2)
+xlim([0.945, 0.98]); ylim([0, 1.0]);
+xlabel('TU'); ylabel('CW p-value');
+title('(c): 1990m1-2019m12 out-of-sample period.');
 
 subplot(2,2,4)
-plot([1,1],get(gca,'ylim'))
+scatter(mc(:,4),mc(:,6),50,'b','filled')
 hold on
-plot(xlim,[0.1,0.1])
-hold on 
-scatter(mc(:,4),mc(:,6))
+plot([1,1],[0, 1.0],'k:')
+plot(xlim,[0.1,0.1],'k--','LineWidth',2)
+xlim([0.945, 0.98]); ylim([0, 1.0]);
+xlabel('TU'); ylabel('DM p-value');
+title('(d): 1990m1-2019m12 out-of-sample period.');
  
